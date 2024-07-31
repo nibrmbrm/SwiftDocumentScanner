ad676d62b3fe2783592ec8717a6608b7b949659c
Nikola Panic <nibrmbrm@icloud.com>
2024-07-31 14:21:53 +0200

    novi paket fajl

--- Package.swift
+++ Package.swift
@@ -1,23 +1,28 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
-    name: "SwiftDocumentScanner",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SwiftDocumentScanner",
-            targets: ["SwiftDocumentScanner"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SwiftDocumentScanner"),
-        .testTarget(
-            name: "SwiftDocumentScannerTests",
-            dependencies: ["SwiftDocumentScanner"]),
-    ]
+   name: "SwiftDocumentScanner",
+   platforms: [
+       .iOS(.v10)
+   ],
+   products: [
+       .library(
+           name: "SwiftDocumentScanner",
+           targets: ["SwiftDocumentScanner"]),
+   ],
+   targets: [
+       .target(
+           name: "SwiftDocumentScanner",
+           dependencies: [],
+           path: "Sources/SwiftDocumentScanner",  // Adjust the path to the source files
+           resources: [
+               .process("Assets")
+           ]),
+       .testTarget(
+           name: "SwiftDocumentScannerTests",
+           dependencies: ["SwiftDocumentScanner"],
+           path: "Tests"
+       ),
+   ]
 )
